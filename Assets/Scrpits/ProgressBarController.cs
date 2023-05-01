8179fa71360b73915b9fc813233d365438e6de24
eraygurbuz <eraygurbuzz06@gmail.com>
2023-05-02 01:28:29 +0300

    Part of the UI is designed

---
+++ Assets/Scrpits/ProgressBarController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProgressBarController : MonoBehaviour
+{
+    float firstDistance, currentDistance;
+    public Slider filledBar;
+    public Transform finishPoint;
+    public Transform playerPoint;
+
+    private void Start()
+    {
+        finishPoint = GameObject.FindGameObjectWithTag("FinishPointForBar").transform;
+        firstDistance = Mathf.Abs(finishPoint.position.z) - Mathf.Abs(playerPoint.position.z);
+
+    }
+
+    void Update()
+    {
+        currentDistance = Mathf.Abs(finishPoint.position.z) - Mathf.Abs(playerPoint.position.z);
+        filledBar.value = (firstDistance - currentDistance) / firstDistance;
+
+    }
+
+}
